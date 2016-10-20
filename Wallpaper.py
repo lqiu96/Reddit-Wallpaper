3c94633f6f91e0e59ab8f5a3c4a2397036699808
Lawrence Qiu <lqiu96@gmail.com>
2016-10-20 13:15:01 -0400

    Add command line arguments for changelog

--- Wallpaper.py
+++ Wallpaper.py
@@ -1,5 +1,7 @@
 import praw
 import os
+import os.path
+import argparse
 from urllib import urlretrieve
 from PIL import Image
 
@@ -7,9 +9,14 @@ SUBREDDIT = ['earthporn', 'spaceporn']
 DEFAULT_DIRECTORY = r'C:\Users\Lawrence\Pictures\Wallpapers\\'
 FAIL_DIRECTORY = r'C:\Users\Lawrence\Pictures\Rejected Wallpapers\\'
 MIN_RATIO, MAX_RATIO = 1.15, 1.6
+
+
 user_agent = 'Wallpaper Retreiver 1.0 by /u/burstoflight'
 reddit = praw.Reddit(user_agent=user_agent)
-
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbosity', action='store_true', help='Print the changelog')
+args = parser.parse_args()
+verbose = args.verbosity
 submissions = []
 for sub in SUBREDDIT:
     submissions += reddit.get_subreddit(sub).get_top(limit=10)
@@ -26,8 +33,17 @@ for file in files:
         width, height = dimension
         ratio = width * 1.0 / height
         if not (MIN_RATIO < ratio < MAX_RATIO):
-            os.rename(file, '{}{}.png'.format(FAIL_DIRECTORY, file[38:]))        
+            if verbose:
+                print 'Rejected: {}'.format(file)
+            rejected_file = '{}{}.png'.format(FAIL_DIRECTORY, file[38:])
+            if os.path.isfile(rejected_file):
+                os.remove(rejected_file)
+            os.rename(file, rejected_file)
+        else:
+            if verbose:
+                print 'Passed: {}'.format(file)
     except IOError:
         f.close()
         os.remove(file)
-        print 'Removed: {}'.format(file)
+        if verbose:
+            print 'Removed: {}'.format(file)
