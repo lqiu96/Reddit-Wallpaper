d5e241029d129bc66e8cddb59eb8f2a68c527cf4
Lawrence Qiu <lqiu96@gmail.com>
2016-10-22 15:59:48 -0400

    Updated for Python 3

--- Wallpaper.py
+++ Wallpaper.py
@@ -2,7 +2,7 @@ import praw
 import os
 import os.path
 import argparse
-from urllib import urlretrieve
+from urllib.request import urlretrieve
 from PIL import Image
 
 SUBREDDIT = {'earthporn', 'spaceporn', 'skyporn', 'waterporn'}
@@ -35,17 +35,17 @@ for sub in SUBREDDIT:
 scores = [post.score for post in submissions]
 scores.sort()
 # Median score determines the minimum score to retrieve images
-median_score = scores[len(scores) / 2] if len(scores) % 2 == 1 else (scores[(len(scores) / 2) - 1] + scores[len(scores) / 2]) / 2
+median_score = scores[len(scores) // 2] if len(scores) % 2 == 1 else (scores[(len(scores) // 2) - 1] + scores[len(scores) // 2]) // 2
 posts = [post for post in submissions if post.score >= median_score]
 # Remove the certain characters that cannot be included in file names
 is_bad_char = lambda x: x in [':', ';', '<', '>', '"', '\\', '/', '|', '?', '*']
-post_titles = map(lambda p: "".join(['' if is_bad_char(l) else l for l in list(p.title)]), posts)
+post_titles = list(map(lambda p: "".join(['' if is_bad_char(l) else l for l in list(p.title)]), posts))
 i = 0
 while i < len(post_titles):
     # Store new name into the post object and encode in utf8
-    posts[i].title = post_titles[i].encode('utf8')
+    posts[i].title = post_titles[i]
     i += 1
-files = map(lambda x: x[0], [urlretrieve(post.url.encode('utf8'), '{}{}.png'.format(DEFAULT_DIRECTORY, post.title)) for post in posts])
+files = list(map(lambda x: x[0], [urlretrieve(post.url, '{}{}.png'.format(DEFAULT_DIRECTORY, post.title)) for post in posts]))
 for file in files:
     try:
         f = open(file, 'rb')
@@ -57,16 +57,16 @@ for file in files:
         # Make sure that the image has good ratio to be put on a Desktop Wallpaper
         if not (MIN_RATIO < ratio < MAX_RATIO):
             if verbose:
-                print 'Rejected: {}'.format(file)
+                print('Rejected: {}'.format(file))
             rejected_file = '{}{}.png'.format(FAIL_DIRECTORY, file[38:])
             if os.path.isfile(rejected_file):
                 os.remove(rejected_file)
             os.rename(file, rejected_file)
         else:
             if verbose:
-                print 'Passed: {}'.format(file)
+                print('Passed: {}'.format(file))
     except IOError:
         f.close()
         os.remove(file)
         if verbose:
-            print 'Removed: {}'.format(file)
+            print('Removed: {}'.format(file))
