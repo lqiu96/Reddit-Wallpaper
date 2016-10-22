9bebb45153c062632f7c538d1ad91cb504e432e7
Lawrence Qiu <lqiu96@gmail.com>
2016-10-22 13:38:28 -0400

    Added comments and updated new subreddits to retreive links

--- Wallpaper.py
+++ Wallpaper.py
@@ -5,16 +5,17 @@ import argparse
 from urllib import urlretrieve
 from PIL import Image
 
-SUBREDDIT = {'earthporn', 'spaceporn'}
+SUBREDDIT = {'earthporn', 'spaceporn', 'skyporn', 'waterporn'}
 DEFAULT_DIRECTORY = r'C:\Users\Lawrence\Pictures\Wallpapers\\'
 FAIL_DIRECTORY = r'C:\Users\Lawrence\Pictures\Rejected Wallpapers\\'
 MIN_RATIO, MAX_RATIO = 1.15, 1.6
+DEFAULT_LIMIT = 25
 time = {
-    'hour': lambda x: x.get_top_from_hour(limit=25),
-    'day': lambda x: x.get_top_from_day(limit=25),
-    'week': lambda x: x.get_top_from_week(limit=25),
-    'month': lambda x: x.get_top_from_month(limit=25),
-    'year': lambda x: x.get_top_from_year(limit=25)
+    'hour': lambda x: x.get_top_from_hour(limit=DEFAULT_LIMIT),
+    'day': lambda x: x.get_top_from_day(limit=DEFAULT_LIMIT),
+    'week': lambda x: x.get_top_from_week(limit=DEFAULT_LIMIT),
+    'month': lambda x: x.get_top_from_month(limit=DEFAULT_LIMIT),
+    'year': lambda x: x.get_top_from_year(limit=DEFAULT_LIMIT)
 }
 
 user_agent = 'Wallpaper Retreiver 1.0 by /u/burstoflight'
@@ -33,12 +34,15 @@ for sub in SUBREDDIT:
     submissions += time.get(args.top, 'day')(reddit.get_subreddit(sub))
 scores = [post.score for post in submissions]
 scores.sort()
+# Median score determines the minimum score to retrieve images
 median_score = scores[len(scores) / 2] if len(scores) % 2 == 1 else (scores[(len(scores) / 2) - 1] + scores[len(scores) / 2]) / 2
 posts = [post for post in submissions if post.score >= median_score]
+# Remove the certain characters that cannot be included in file names
 is_bad_char = lambda x: x in [':', ';', '<', '>', '"', '\\', '/', '|', '?', '*']
 post_titles = map(lambda p: "".join(['' if is_bad_char(l) else l for l in list(p.title)]), posts)
 i = 0
 while i < len(post_titles):
+    # Store new name into the post object and encode in utf8
     posts[i].title = post_titles[i].encode('utf8')
     i += 1
 files = map(lambda x: x[0], [urlretrieve(post.url.encode('utf8'), '{}{}.png'.format(DEFAULT_DIRECTORY, post.title)) for post in posts])
@@ -50,6 +54,7 @@ for file in files:
         f.close()
         width, height = dimension
         ratio = width * 1.0 / height
+        # Make sure that the image has good ratio to be put on a Desktop Wallpaper
         if not (MIN_RATIO < ratio < MAX_RATIO):
             if verbose:
                 print 'Rejected: {}'.format(file)
